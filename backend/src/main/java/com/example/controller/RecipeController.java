7890ea01fe85844842b7e1b76a4613eb8800b75a
eleonora2003 <eleonorastankovska25@gmail.com>
2024-11-26 16:30:34 +0100

    update recipes

--- backend/src/main/java/com/example/controller/RecipeController.java
+++ backend/src/main/java/com/example/controller/RecipeController.java
@@ -4,6 +4,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,9 +14,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -23,15 +26,12 @@ import com.example.entity.FoodType;
 import com.example.entity.Recipes;
 import com.example.service.RecipeService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+import com.example.repository.RecipesRepo;
 import jakarta.servlet.http.HttpServletRequest;
-<<<<<<< HEAD
 import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.RequestParam;
 import com.example.service.ExcelExportService;
-=======
->>>>>>> 7d20cb828c04904d3833e0694affb5d402e66dff
 
 @CrossOrigin(origins = "http://localhost:3000") 
 @RestController
@@ -117,7 +117,6 @@ private String saveImageFile(MultipartFile file) throws Exception {
     }
 
     
-<<<<<<< HEAD
 @Autowired
     private RecipesRepo recipesRepository;
 
@@ -149,8 +148,6 @@ private String saveImageFile(MultipartFile file) throws Exception {
         // Return success response
         return ResponseEntity.ok("Updated");
     }
-=======
->>>>>>> 7d20cb828c04904d3833e0694affb5d402e66dff
 
      private final ExcelExportService excelExportService;
     public RecipeController(ExcelExportService excelExportService) {
